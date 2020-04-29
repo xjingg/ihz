72893ebce85d500f19e4ee8025f6b33dad930a52
Evelyn <junecug@gmail.com>
2020-04-29 19:18:32 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import NavHeader from "./components/NavHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <NavHeader />
+    </div>
+  );
+}
+
+export default App;
